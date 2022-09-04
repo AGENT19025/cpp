895766c760895c0f6e46b15181977ffa12f797d6
kalla vishwas uday kiran <kallavishwasudaykiran@gmail.com>
2022-09-05 01:31:26 +0530

    Add files via upload

---
+++ reversing  a string.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int32_t main()
+{
+    string s;
+    getline(cin,s);
+    string r=s;
+    reverse(r.begin(),r.end());
+    cout<<r;
+    return 0;
+}
